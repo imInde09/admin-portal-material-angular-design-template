34895a2ffe4dbb81ef829f0dfa80a64f9ade07c1
Prathamesh Inde <prathameshinde86@gmail.com>
2023-08-21 18:55:07 +0530

    products -> category page added.

--- src/app/admin/products/category/category.component.ts
+++ src/app/admin/products/category/category.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
-
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements AfterViewInit {
+  displayedColumns: string[] = [ 'name', 'details','parent_category','status','actions'];
+  dataSource: MatTableDataSource<UserData>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 30}, (_, k) => createNewUser(k + 1));
 
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  onedit(item:any){
+    console.log(item.id+" "+item.name+" "+item.details+" "+item.parent_category+" "+item.status);
+  }
+  ondelete(){
+    console.log("delete");
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+export interface UserData {
+  id: string;
+  name: string;
+  details: string;
+  status: string;
+  parent_category: string;
+}
+
+/** Constants used to fill up our data base. */
+const DETAILS: string[] = [
+  'laser-wristbands',
+  'thermal-wristbands',
+  'sheild-wristbands',
+  'industrial-label-printers','mobile-printers','scanner'
+];
+const CATERGORY: string[] = [
+  'IDENTIFICATION CARD PRINTERS','NONE','LABEL PRINTER','PATIENT ID WRISTBANDS'
+];
+const STATUS: string[] = [
+  'active','inactive'
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    details: DETAILS[Math.round(Math.random() * (DETAILS.length - 1))],
+    parent_category: CATERGORY[Math.round(Math.random() * (CATERGORY.length - 1))],
+    status: STATUS[Math.round(Math.random() * (STATUS.length - 1))],
+  };
 }
--- src/app/admin/products/products.module.ts
+++ src/app/admin/products/products.module.ts
@@ -6,13 +6,27 @@ import { ProductsComponent } from './products.component';
 import { CategoryComponent } from './category/category.component';
 import { ApplicationsComponent } from './applications/applications.component';
 import { PricingImportComponent } from './pricing-import/pricing-import.component';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {NgIf} from '@angular/common';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule, MatListItem, MatListItemLine} from '@angular/material/list';
+import {MatList} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { MatFormField } from '@angular/material/form-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatMenuModule} from '@angular/material/menu';
 @NgModule({
   declarations: [
     ListComponent,ProductsComponent, CategoryComponent, ApplicationsComponent, PricingImportComponent
   ],
   imports: [
-    CommonModule,RouterModule
+    CommonModule,RouterModule,MatIconModule,MatButtonModule,MatToolbarModule,MatSidenavModule,NgIf,MatListModule,MatExpansionModule,
+    MatTableModule,MatPaginatorModule,MatFormFieldModule,MatSortModule,MatMenuModule
   ]
 })
 export class ProductsModule { }
