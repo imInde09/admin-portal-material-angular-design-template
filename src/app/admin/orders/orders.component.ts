fbb348d939d3d5cf7412aa472fadee3bbaecd9c5
Prathamesh Inde <prathameshinde86@gmail.com>
2023-08-22 20:21:39 +0530

    Order Page Added and UI updates in News Page

--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
-export class OrdersComponent {
+export class OrdersComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name','date', 'status','total','actions'];
+  dataSource: MatTableDataSource<UserData>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor() {
+    // Create 100 users
+    const users = Array.from({length: 30}, (_, k) => createNewUser(k + 1));
 
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  onedit(item:any){
+    console.log(item.id+item.name+item.date+item.progress+item.item);
+  }
+  ondelete(){
+    console.log("delete");
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+export interface UserData {
+  id: string;
+  name: string;
+  date: string;
+  progress: string;
+  status: string;
+  total: number;
+}
+
+/** Constants used to fill up our data base. */
+const STATUS: string[] = [
+  'pending',
+  'processing',
+  'completed',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    date: Date.now().toString(),
+    progress: Math.round(Math.random() * 100).toString(),
+    status: STATUS[Math.round(Math.random() * (STATUS.length - 1))],
+    total: Math.round(Math.random() * 1000),
+  };
 }
