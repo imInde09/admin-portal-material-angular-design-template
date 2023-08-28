d8c75b6a17efb002a69bbbf1e616f63d2fca58fd
Prathamesh Inde <prathameshinde86@gmail.com>
2023-08-28 14:37:57 +0530

    profiile page updated with form.

--- src/app/admin/profile/profile.component.ts
+++ src/app/admin/profile/profile.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+formData={
+  name:'',
+  email:'',
+}
+updatepassword={
+  oldPassword:'',
+  newPassword:'',
+  confirmPassword:'',
+}
+  updateProfile(){
+    console.log("update profile");
+    console.log(this.formData);
+  }
+  updatePassword(){
+    console.log("update password");
+    console.log(this.updatepassword);
+  }
 }
